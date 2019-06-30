081d1098d91700e351ef8779677bb8ac45b4106f
Dominik Madarász <zaklaus@outlook.com>
2019-06-30 15:40:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#DFUMods 
+
+This is a collection of mods I've made for [Daggerfall Unity](https://dfworkshop.net/).
+
+## Links
+- [Toggle Mount](#)
+
+## Copyright
+
+Mods are licensed under the MIT license.
---
+++ ToggleMount/ToggleMount.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+using DaggerfallWorkshop.Game.UserInterfaceWindows;
+using System;
+using DaggerfallWorkshop;
+
+namespace DaggerfallMod
+{
+    public class ToggleMount : MonoBehaviour
+    {
+        private Mod mod;
+        private KeyCode toggleKey = KeyCode.G;
+
+        public void Setup(Mod mod)
+        {
+            this.mod = mod;
+            mod.IsReady = true;
+
+            var settings = mod.GetSettings();
+            string keyName = settings.GetValue<string>("General", "Hotkey");
+
+            var key = (KeyCode)Enum.Parse(typeof(KeyCode), keyName);
+
+            if (Enum.IsDefined(typeof(KeyCode), key))
+            {
+                toggleKey = key;
+            } else
+            {
+                DaggerfallUnity.LogMessage("ToggleMount: Hotkey could not be processed! Falling back to 'G'!", true);
+            }
+        }
+        
+        void Update()
+        {
+            if (GameManager.Instance.StateManager.CurrentState == StateManager.StateTypes.Game)
+            {
+                if (Input.GetKeyDown(toggleKey))
+                {
+                    if (GameManager.Instance.IsPlayerInside)
+                    {
+                        DaggerfallUI.AddHUDText(HardStrings.cannotChangeTransportationIndoors);
+                    }
+                    else
+                    {
+                        if (GameManager.Instance.PlayerController.isGrounded)
+                        {
+                            GameManager.Instance.TransportManager.ToggleMount();
+                        }
+                    }
+                }
+            }
+        }
+        
+        [Invoke(StateManager.StateTypes.Game)]
+        public static void Init(InitParams initParams)
+        {
+            var instance = GameManager.Instance.PlayerObject.AddComponent<ToggleMount>();
+            instance.Setup(initParams.Mod);
+        }
+    }
+}
